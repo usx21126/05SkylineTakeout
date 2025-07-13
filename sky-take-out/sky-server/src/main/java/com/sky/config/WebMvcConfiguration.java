87fd608ce38a606570c75373f490f93a63e1fac8
2746150991 <2746150991@qq.com>
2025-07-13 13:35:48 +0800

    day01 submit

--- sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -30,6 +30,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      *
      * @param registry
      */
+    @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
