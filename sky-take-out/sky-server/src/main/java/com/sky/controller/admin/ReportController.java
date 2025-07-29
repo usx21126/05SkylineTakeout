c312de0cd276575c93eb436b03e144537f5443b0
2746150991 <2746150991@qq.com>
2025-07-29 23:57:28 +0800

    final commit

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -84,5 +85,14 @@ public class ReportController {
         log.info("begin:{}, end:{}", begin, end);
         SalesTop10ReportVO salesTop10ReportVO = reportService.top10(begin,end);
         return Result.success(salesTop10ReportVO);
+    }
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出Excel报表接口")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 }
